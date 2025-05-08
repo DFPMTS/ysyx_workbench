280655125ef47999db03285ad9cdfd2b82945ef1
dfpmts <hanlt@shanghaitech.edu.cn>
2025-05-08 19:24:29 +0800

    add zifencei to riscv32-npc.mk

--- abstract-machine/scripts/riscv32-npc.mk
+++ abstract-machine/scripts/riscv32-npc.mk
@@ -1,4 +1,4 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/npc.mk
-COMMON_CFLAGS += -march=rv32ima_zicsr -mabi=ilp32  # overwrite
+COMMON_CFLAGS += -march=rv32ima_zicsr_zifencei -mabi=ilp32  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
