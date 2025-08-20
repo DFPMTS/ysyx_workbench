d2e359230efa8793e1c198dec5bd0397de34896d
dfpmts <hanlt@shanghaitech.edu.cn>
2025-08-20 08:44:34 +0800

    reduce PHT_INDEX_LEN by 1 (to 10)

--- npc/Core/src/utils/CoreConfig.scala
+++ npc/Core/src/utils/CoreConfig.scala
@@ -83,7 +83,7 @@ trait HasCoreParameters extends HasDecodeConfig {
   def BTB_TAG = XLEN - BTB_INDEX_LEN - log2Up(FETCH_WIDTH * 4) // ! C-extension
 
   // * PHT Size (of a single Bank)
-  def PHT_INDEX_LEN = 11
+  def PHT_INDEX_LEN = 10
   def PHT_SIZE = 1 << PHT_INDEX_LEN
 
   // * Global History Length
